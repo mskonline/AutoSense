8e40a40c16a8f8c9deb955eb27b3b0e05738da08
annapusai573 <annapureddy.sai.kumar.94@gmail.com>
2016-10-08 17:47:30 -0500

    sample commit

--- src/app/AutoSense/app/src/main/java/org/autosense/MainActivity.java
+++ src/app/AutoSense/app/src/main/java/org/autosense/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); // sample commit
         setContentView(R.layout.activity_main);
     }
 }
